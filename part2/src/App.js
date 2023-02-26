a08050e1a7a07665e2a1e47097c432343b37ee00
RayChromium <raychromium@outlook.com>
2023-02-26 23:12:21 +0800

    e2.9

--- part2/src/App.js
+++ part2/src/App.js
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '12-23-4567' }
+    { name: 'Arto Hellas', number: '12-23-4567', id: 1  },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }  
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('');
+  const [searchTarget, setSearchTarget] = useState('');
 
   const addEntry = (e) => {
     e.preventDefault();
@@ -13,6 +17,7 @@ const App = () => {
     const newPerson = {
       name: newName,
       number: newNumber,
+      id: persons.length + 1,
     }
 
     if( persons.findIndex( person => person.name === newName ) === -1 ) {
@@ -34,9 +39,16 @@ const App = () => {
     setNewNumber(e.target.value);
   }
 
+  const handleSearchTarget = (e) => {
+    console.log('searching for:', e.target.value);
+    setSearchTarget(e.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with name: <input onChange={handleSearchTarget}/>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameInput}/>
@@ -47,7 +59,17 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map( person => <p key={person.name}>{person.name}: {person.number}</p> )}
+      {persons.map( person => {
+        if(searchTarget === '') {
+          return <p key={person.id}>{person.name}: {person.number}</p>
+        }
+
+        if( person.name.toLocaleLowerCase().indexOf(searchTarget) !== -1 ) {
+          return <p key={person.id}>{person.name}: {person.number}</p>
+        }
+        //it works?
+        return [];
+      } )}
     </div>
   )
 }
