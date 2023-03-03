ca48b3e895390bfd2f6b47d43594ea1841cf0f61
RayChromium <raychromium@outlook.com>
2023-03-03 21:32:57 +0800

    part3.a receive post data

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 let notes = [
     {
@@ -53,6 +54,32 @@ app.delete( '/api/notes/:id', (request, response) => {
     // 204 No Content
     // The 204 (No Content) status code indicates that the server has successfully fulfilled the request and that there is no additional content to send in the response content.
     response.status(204).end();
+} );
+
+const generateId = () => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map( n => n.id )) : 0 ;
+    return maxId + 1;
+}
+
+app.post( '/api/notes', (request, response) => {
+    const body = request.body;
+    
+    console.log('body:', body);
+    // aka: body.content is undefined:
+    if(!body.content) {
+        return response.status(400).json({ error: 'content missing' });
+    }
+
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId()
+    };
+
+    notes = notes.concat(note);
+    
+    response.json(note);
 } );
 
 const PORT = 3001;
