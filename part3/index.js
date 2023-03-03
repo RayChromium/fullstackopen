e17aab6bf6d99a0cdf77d9c2b385fd968ed97c31
RayChromium <raychromium@outlook.com>
2023-03-03 14:34:29 +0800

    p3.a simple web server with node http module

--- part3/index.js
+++ part3/index.js
@@ -1,1 +1,11 @@
-console.log('hello world!');
+const http = require('http');
+
+const app = http.createServer( ( request, respond ) => {
+    respond.writeHead( 200, { 'Content-Type' : 'text/plain' } );
+    respond.end( 'Hello World!' );
+} );
+
+const PORT = 3001;
+
+app.listen(PORT);
+console.log( `Server running on port ${PORT}` );
