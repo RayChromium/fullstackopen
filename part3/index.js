41f008862a4d0dccd17a6b6983e3628b4fcc48fe
RayChromium <raychromium@outlook.com>
2023-03-05 23:36:36 +0800

    middleware

--- part3/index.js
+++ part3/index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const requestLogger = (request, response, next) => {
+    console.log('Method: ', request.method );
+    console.log( 'Path: ', request.path );
+    console.log( 'Body: ', request.body );
+    console.log('---');
+    next();
+}
+
+app.use(requestLogger);
+
 let notes = [
     {
       id: 1,
@@ -81,6 +91,12 @@ app.post( '/api/notes', (request, response) => {
     
     response.json(note);
 } );
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error:'Unknown Endpoint'});
+}
+
+app.use( unknownEndpoint );
 
 const PORT = 3001;
 
