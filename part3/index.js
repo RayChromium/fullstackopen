25870e971e60bf0f1764acf1b63cd1be4b6b7cac
RayChromium <raychromium@outlook.com>
2023-03-16 20:09:39 +0800

    part3d: mongoose field validation

--- part3/index.js
+++ part3/index.js
@@ -17,24 +17,6 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger);
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-]
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!<h1/>');
 });
@@ -83,19 +65,18 @@ app.put('/api/notes/:id', (request, response, next) => {
         return response.status(400).json({ error: 'content missing' });
     }
 
-    const note = {
-        content : body.content,
-        important : body.important || false,
-    }
+    const {content, important} = body;
 
-    Note.findByIdAndUpdate(request.params.id, note, { new:true })
+    Note.findByIdAndUpdate(request.params.id, 
+                           {content, important}, 
+                           { new:true, runValidators:true, context: 'query' })
         .then( updatedNote => {
             response.json(updatedNote);
         } )
         .catch( error => next(error) )
 })
 
-app.post( '/api/notes', (request, response) => {
+app.post( '/api/notes', (request, response, next) => {
     const body = request.body;
     
     console.log('body:', body);
@@ -110,9 +91,12 @@ app.post( '/api/notes', (request, response) => {
         important: body.important || false,
     });
 
-    note.save().then( savedNote => {
+    note.save()
+    .then( savedNote => {
         response.json(savedNote);
-    } );
+    } )
+    .catch( error => next(error) )
+    ;
 } );
 
 // the handler dealing with unknown endpoint cannot come before routes
@@ -128,6 +112,8 @@ const errorHandler = ( error, request, response, next ) => {
 
     if(error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' });
+    } else if( error.name === 'ValidationError' ) {
+        return response.status(400).json({error: error.message});
     }
 
     next(error);
