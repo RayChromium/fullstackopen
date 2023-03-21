3b9ec957577c09cfee8fa8f3072c12161eca3c75
RayChromium <raychromium@outlook.com>
2023-03-22 00:03:55 +0800

    e4.2: utils middleware

--- part4-bloglist/index.js
+++ part4-bloglist/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
+const middleware = require('./utils/middleware');
 const Blog = require('./models/blog');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -27,6 +28,8 @@ mongoose.connect(mongoUrl)
 app.use(cors());
 app.use(express.json());
 
+app.use(middleware.requestLogger);
+
 app.get( '/api/blogs', (request, response) => {
     Blog
         .find({})
@@ -44,6 +47,11 @@ app.post('/api/blogs', (request, response) => {
             response.status(201).json(result);
         });
 });
+
+// unknown endpoint router handler must come after all valid routers
+app.use(middleware.unknownEndpoint);
+// error handler comes last
+app.use(middleware.errorHandler);
 
 const PORT = config.PORT || 3003;
 app.listen(PORT, () => {
