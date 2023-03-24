2d5019ef46472b4b9066934393c57c6fdc564ff2
RayChromium <raychromium@outlook.com>
2023-03-24 21:34:00 +0800

    e4.4: calculate totalLikes test

--- part4-bloglist/utils/list_helper.js
+++ part4-bloglist/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
     return 1;
 };
 
+const totalLikes = (blogs) => {
+    const likes = blogs.reduce( (sum, blog) => {
+        return sum + blog.likes;
+    }, 0 );
+    return blogs.length === 0
+        ? 0
+        : likes;
+};
+
 module.exports = {
     dummy,
+    totalLikes,
 };
