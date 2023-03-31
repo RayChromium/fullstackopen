a4bc104789b9a0e420e379dcd734b4a076e5c3f4
RayChromium <raychromium@outlook.com>
2023-03-31 20:07:55 +0800

    part4.b: supertest(could be affected by network)

--- part3/test/note_api.test.js
+++ part3/test/note_api.test.js
@@ -1,8 +1,46 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const Note = require('../models/note');
 
 const api = supertest(app);
+
+const initialNotes = [
+    {
+        content: 'HTML is easy',
+        important: false,
+    },
+    {
+        content: 'Browser can execute only JavaScript',
+        important: true,
+    },
+];
+
+beforeEach( async () =>{
+    await Note.deleteMany({});
+    let noteObject = new Note(initialNotes[0]);
+    await noteObject.save();
+    noteObject = new Note(initialNotes[1]);
+    await noteObject.save();
+}, 100000);
+
+// beforeEach(() => {
+//     // await Note.deleteMany({})
+//     // let noteObject = new Note(initialNotes[0])
+//     // await noteObject.save()
+//     // noteObject = new Note(initialNotes[1])
+//     // await noteObject.save()
+//     Note.deleteMany({})
+//         .then( () => {
+//             let noteObject = new Note(initialNotes[0]);
+//             noteObject.save()
+//                 .then( () => {
+//                     noteObject = new Note(initialNotes[1]);
+//                     noteObject.save();
+//                 } );
+//         } )
+//         .catch(error => console.error(error));
+// });
 
 test('notes are returned as json', async () => {
     await api
@@ -10,6 +48,18 @@ test('notes are returned as json', async () => {
         .expect(200)
         .expect('Content-Type', /application\/json/);
 }, 100000);
+
+test('there are two notes', async () => {
+    const response = await api.get('/api/notes');
+
+    expect(response.body).toHaveLength(initialNotes.length);
+});
+
+test('a specific note is within the returned notes', async () => {
+    const response = await api.get('/api/notes');
+    const contents = response.body.map( r => r.content );
+    expect(contents).toContain('Browser can execute only JavaScript');
+});
 
 afterAll( async () => {
     await mongoose.connection.close();
