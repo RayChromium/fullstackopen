9e4e42fd6f64184b0067005dec546ac4b20b4dc9
RayChromium <raychromium@outlook.com>
2023-04-01 14:21:15 +0800

    part4.b: async/await in backend example

--- part3/controllers/notes.js
+++ part3/controllers/notes.js
@@ -2,10 +2,9 @@ const notesRouter = require('express').Router();
 const Note = require('../models/note');
 const logger = require('../utils/logger');
 
-notesRouter.get('/', (request, response) => {
-    Note.find({}).then( note => {
-        response.json(note);
-    } );
+notesRouter.get('/', async (request, response) => {
+    const notes = await Note.find({});
+    response.json(notes);
 });
 
 notesRouter.get('/:id', (request, response, next) => {
