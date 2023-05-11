949149237713faeba7c2c9693446a3545724d71f
RayChromium <raychromium@outlook.com>
2023-05-12 00:03:33 +0800

    e4.9: test for blog contains  id

--- part4-bloglist/test/list_api.test.js
+++ part4-bloglist/test/list_api.test.js
@@ -1,8 +1,26 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
+const helper = require('./test_helper');
+const Blog = require('../models/blog');
 const app = require('../app');
 
 const api = supertest(app);
+
+beforeEach( async () => {
+    await Blog.deleteMany({});
+    for( let blog of helper.initialBlogs ) {
+        let blogObject = new Blog(blog);
+        await blogObject.save();
+    }
+} , 100000);
+
+test( 'e4.9: blogs contain a property named id', async () => {
+    const response = await api.get('/api/blogs');
+    // console.log(response.body);
+    for( let blog of response.body ) {
+        expect(blog.id).toBeDefined();
+    }
+} );
 
 test( 'blogs are returned as json', async () => {
     await api
