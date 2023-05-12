fc4c2da163ff0ac3322dea484e25546e71a2ce37
RayChromium <raychromium@outlook.com>
2023-05-12 21:35:12 +0800

    e4.10: test for posting new blog

--- part4-bloglist/test/list_api.test.js
+++ part4-bloglist/test/list_api.test.js
@@ -20,7 +20,29 @@ test( 'e4.9: blogs contain a property named id', async () => {
     for( let blog of response.body ) {
         expect(blog.id).toBeDefined();
     }
-} );
+}, 100000 );
+
+test( 'e4.10: ok to add a new blog', async () => {
+    const newBlog = {
+        title: 'A newly added blog (test)',
+        author: 'Cai Rui',
+        url: 'https://pornhub.com/',
+        likes: 98,
+    };
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/ );
+
+    const response = await api.get('/api/blogs');
+    const titles = response.body.map(b => b.title);
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1);
+    expect(titles).toContain('A newly added blog (test)');
+
+}, 100000 );
 
 test( 'blogs are returned as json', async () => {
     await api
