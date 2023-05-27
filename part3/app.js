a9d86c87b62a1c1dc76a1d3a1fc5e6c851db24e6
RayChromium <raychromium@outlook.com>
2023-05-27 22:48:32 +0800

    part4.c: basic user create router

--- part3/app.js
+++ part3/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 require('express-async-errors');
 const cors = require('cors');
 const notesRouter = require('./controllers/notes');
+const usersRouter = require('./controllers/users');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
 
