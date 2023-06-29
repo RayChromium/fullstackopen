7da4d394ecfc9fc4aec605440c1937f36b07bf90
RayChromium <raychromium@outlook.com>
2023-06-29 14:10:01 +0800

    e4.19: limit blog adding with token

--- part4-bloglist/app.js
+++ part4-bloglist/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
 const middleware = require('./utils/middleware');
--- part4-bloglist/controllers/blogs.js
+++ part4-bloglist/controllers/blogs.js
@@ -1,35 +1,44 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 const logger = require('../utils/logger');
 
-blogsRouter.get( '/', (request, response, next) => {
-    Blog
-        .find({})
-        .populate('user')
-        .then( blogs => {
-            response.json(blogs);
-        } )
-        .catch(error => next(error));
+const getTokenFrom = request => {
+    const authorization = request.get('authorization');
+    if( authorization &&  authorization.startsWith('Bearer') ) {
+        return authorization.replace( 'Bearer', '' );
+    }
+    return null;
+};
+
+blogsRouter.get( '/', async (request, response) => {
+    const blogs = await Blog.find({}).populate('user');
+    response.json(blogs);
 } );
 
-blogsRouter.get('/:id', (request, response, next) =>{
-    Blog.findById( request.params.id )
-        .then( returnedPost => {
-            logger.info(returnedPost);
-            response.json(returnedPost);
-        } )
-        .catch( error => next(error) );
+blogsRouter.get('/:id', async (request, response) =>{
+    const returnedPost = await Blog.findById( request.params.id );
+    logger.info(returnedPost);
+    response.json(returnedPost);
 });
 
-blogsRouter.post('/', (request, response, next) => {
-    const blog = new Blog(request.body);
+blogsRouter.post('/', async (request, response) => {
+    logger.info(request.body);
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+    if(!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' });
+    }
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result);
-        })
-        .catch(error => next(error));
+    const user = await User.findById(decodedToken.id);
+    const blog = new Blog({
+        title: request.body.title,
+        author: request.body.author,
+        url: request.body.url,
+        user: user._id
+    });
+    const result = await blog.save();
+    response.status(201).json(result);
 });
 
 module.exports = blogsRouter;
