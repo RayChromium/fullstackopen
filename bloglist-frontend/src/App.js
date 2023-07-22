6ad5c9290434484fcd99b88a15f35cb916a73fd0
RayChromium <raychromium@outlook.com>
2023-07-22 15:40:51 +0300

    e5.3: create a new

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -8,6 +8,9 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -32,6 +35,25 @@ const App = () => {
     window.localStorage.removeItem('loggedBloglistUser')
     setUser(null)
     blogService.removeToken()
+  }
+
+  const createNewBlog = async event => {
+    event.preventDefault()
+    const newObject = {
+      title: title,
+      author: author,
+      url: url,
+    }
+    try{
+      const response = await blogService.create(newObject)
+      console.log(response)
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      setBlogs( blogs.concat(response) )
+    } catch( exception ) {
+      console.error(exception)
+    }
   }
 
   useEffect(() => {
@@ -80,11 +102,44 @@ const App = () => {
 
   return (
     <div>
+      <h2>blogs</h2>
       <div>
         <h3>Logged in User: {user.username}</h3>
-        <button on onClick={handleLogout}>Log out</button>
+        <button onClick={handleLogout}>Log out</button>
       </div>
-      <h2>blogs</h2>
+      <div>
+        <h3>Create New</h3>
+        <form onSubmit={createNewBlog}>
+          <div>
+            title
+              <input 
+              type='text' 
+              value={title} 
+              name='Title' 
+              onChange={({ target }) => setTitle(target.value)} 
+              />
+          </div>
+          <div>
+            author
+              <input 
+              type='text' 
+              value={author} 
+              name='Author' 
+              onChange={ ( {target} ) => setAuthor(target.value) } 
+              />
+          </div>
+          <div>
+            url
+              <input 
+              type='text' 
+              value={url} 
+              name='Url' 
+              onChange={ ( {target} ) => setUrl(target.value) } 
+              />
+          </div>
+          <button type='submit'>create</button>
+        </form>
+      </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
