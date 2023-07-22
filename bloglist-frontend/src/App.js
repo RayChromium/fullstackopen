f61292e838635bcf1631d11216e54cca270a2103
RayChromium <raychromium@outlook.com>
2023-07-22 14:37:59 +0300

    e5.1: login to bloglist

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,15 +1,65 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginServeice from './services/login'
+import login from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log('logging in with ', username, password)
+
+    try{
+      const user = await loginServeice.login( {
+        username, password,
+      } )
+      setUser(user);
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.error(exception);
+    }
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+              <input 
+              type='text' 
+              value={username} 
+              name='Username' 
+              onChange={({ target }) => setUsername(target.value)} 
+              />
+          </div>
+          <div>
+            password
+              <input 
+              type='password' 
+              value={password} 
+              name='Password' 
+              onChange={ ( {target} ) => setPassword(target.value) } 
+              />
+          </div>
+          <button type='submit'>Login</button>
+        </form>
+      </div>
+    )
+  }
 
   return (
     <div>
